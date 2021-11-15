8613724ce0745c80fd9475d248e336442d8a811e
Szilard Kalosi <kalosi.szilard@gmail.com>
2021-11-15 19:30:16 +0100

    feat: temp

--- best_lib/bum/puff.py
+++ best_lib/bum/puff.py
@@ -17,3 +17,7 @@ def age(age: int):
 # calculate feet to centimeters
 def feet_to_cm(feet: int):
     return feet * 30.48
+
+
+def fahrenheit_to_celsius(fahrenheit: int):
+    return (fahrenheit - 32) * 5 / 9
